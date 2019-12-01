12945b9ac5730ca17e228c415131aa1a2ebb356f
john L <johnleavell@gmail.com>
2019-12-01 03:15:37 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,6 @@ class Person
 
   def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:
     @name = name
-    @birthday = birthday
     @hair_color = hair_color
     @eye_color = eye_color
     @height = height
@@ -17,5 +16,5 @@ class Person
     @pant_length = pant_length
     @pant_width = pant_width
   end
-  
+
 end
